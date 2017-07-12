fa587eaaeecfdfb9af2dab291fb33eca084f8675
gamegenius <liuchar@gmail.com>
2017-07-12 22:46:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-var exec = require('child_process').exec;
-//需要执行的命令字符串
-var cli = 'npm install -g peerflix-server';
-exec(cli,{encoding:'utf8'},function (err,stdout,stderr){
-    if (err){
-        console.log(err);
-        return;
-    }
-    console.log('stdout'+stdout);
-    console.log('stderr'+stderr);
-})
-cli = 'PORT=8080 peerflix-server';
-exec(cli,{encoding:'utf8'},function (err,stdout,stderr){
-    if (err){
-        console.log(err);
-        return;
-    }
-    console.log('stdout'+stdout);
-    console.log('stderr'+stderr);
-})
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8081);
