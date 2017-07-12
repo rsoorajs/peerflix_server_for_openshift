b29e2cc584e06b86e0ef4cabf3bde8cd1968d64e
gamegenius <liuchar@gmail.com>
2017-07-12 23:06:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
   fs.readFile('index.html', function(err, data) {
@@ -6,4 +6,5 @@ http.createServer(function (req, res) {
     res.write(data);
     return res.end();
   });
-}).listen(8081);
+}).listen(8081);*/
+console.log("Ok.");
