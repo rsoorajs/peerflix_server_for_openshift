f9b25c7a10d5821e24a9197d9903e97dca5dc090
gamegenius <liuchar@gmail.com>
2017-08-18 12:57:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8081);
+var spawn = require('child_process').spawn;
+var start = spawn('sh', ['start.sh','']);
+
+start.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+start.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+start.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
